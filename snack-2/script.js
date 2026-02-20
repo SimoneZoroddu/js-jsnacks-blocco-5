7a569a2f0b388dee12cfbe2f91435243dd5f002d
SimoneZoroddu <lenviur@gmail.com>
2026-02-20 13:44:35 +0100

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,7 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+let nameString = ''
+people.forEach(name => nameString += name.name + ", ")
+console.log(nameString);
