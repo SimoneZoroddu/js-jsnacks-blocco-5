636db4340e4703488acc2bf2281e4b47eed63809
SimoneZoroddu <lenviur@gmail.com>
2026-02-20 13:46:37 +0100

    snack 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,9 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numberPlus1 = []
+
+numbers.forEach(number => numberPlus1.push(number + 1))
+
+console.log(numberPlus1);
